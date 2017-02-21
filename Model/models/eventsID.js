1abece6541c1e05712ae066e70f9a6efc993e9bc
Jyothi <jyothigangula@gmail.com>
2017-02-21 11:22:35 +0100

    Model Upadted

---
+++ Model/models/eventsID.js
@@ -0,0 +1,6 @@
+ schema
+        
+        {
+            eventid:{ type : Number,required : true,unique : true}// is required, is unique
+           
+        }
---
+++ Model/models/serverModelTest.js
@@ -0,0 +1,11 @@
+var eventsModel = require("./model")
+eventsModel.setSchema({events schema})
+
+var sessionsModel = require("./model")
+sessionsModel.setSchema({sessions schema})
+
+var shoppingModel = require("./model")
+shoppingModel.setSchema({shopping schema})
+
+
+eventsModel.delete(obj, cb)
