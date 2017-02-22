60290d7a90fe69835136283fa974bf32aefa4f42
mardyr <marilena.dyrstad@gmail.com>
2017-02-22 10:24:16 +0100

    Added hardcoded events to Events db

--- controllers/server.js
+++ controllers/server.js
@@ -22,6 +22,33 @@ var Events = new Model({
     price: {type: "number"}
    })
 
+Events.db = [
+    {
+        id: 1,
+        name: "SuperCodeCamp",
+        startDate: '27/05/2017, 08:00 AM',
+        endDate: '27/08/2017, 08:00 PM',
+        subjects: ['Node.js', 'express.js', 'mongoDB'],
+        type: ['Full stack', 'Three months'],
+        image: 'https://i2.wp.com/www.barcelonacodeschool.com/wp-content/uploads/2016/04/students-in-classroom.jpg?zoom=1.5&fit=564%2C388',
+        description: 'This is a code camp',
+        price: 3000
+    },
+    {
+        id: 2,
+        name: "Workshop",
+        startDate: '27/05/2017, 08:00 AM',
+        endDate: '28/05/2017, 08:00 PM',
+        subjects: ['Node.js', 'express.js', 'mongoDB', 'javascript'],
+        type: ['Full stack', 'Two days'],
+        image: 'https://i2.wp.com/www.barcelonacodeschool.com/wp-content/uploads/2016/04/students-in-classroom.jpg?zoom=1.5&fit=564%2C388',
+        description: 'This is a workshop',
+        price: 300
+    }
+]
+
+console.log('Events after hardcoding: ', Events)
+
 var Users = new Model({
     firstName: {type: "string"},
     lastName: {type: "string"},
